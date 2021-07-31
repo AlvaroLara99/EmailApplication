ac5ba7298851084218a968c5f42c35e2aa2d16e6
Alvaro Lara <alvarolara712@gmail.com>
2021-07-31 01:21:48 -0500

    Created new java project

---
+++ src/EmailApp/Email.java
@@ -0,0 +1,25 @@
+package EmailApp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmaill;
+
+	//Constructor to receive the fist name and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	//Ask for department
+	
+	//Generate random password
+	
+	//Set the mailbox capacity
+	
+	//Set the alternate email
+	
+	//Change the password
+}
